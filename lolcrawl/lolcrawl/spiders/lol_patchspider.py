b80d9317dfac4addad5b9ec1ff20104115b1eab6
joe <lee.joseph11@gmail.com>
2017-01-23 14:33:18 -0800

    patch crawler finished

--- lolcrawl/lolcrawl/spiders/lol_patchspider.py
+++ lolcrawl/lolcrawl/spiders/lol_patchspider.py
@@ -1,48 +1,68 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from datetime import datetime
+import time
+import re
 
 
 class LolSpider(CrawlSpider):
     name = "patch_data"
     allowed_domains = ['nerfplz.com']
-    # start_urls = ['http://www.nerfplz.com/search/label/Patch Notes?max-results=50']
-    start_urls = ['http://www.nerfplz.com/search/label/Patch Notes?max-results=5']
+    start_urls = ['http://www.nerfplz.com/search/label/Patch Notes?max-results=40']
     rules = (
         Rule(LinkExtractor(restrict_xpaths='//span[@class="rmlink"]'),
              callback='parse_patch'),
         Rule(LinkExtractor(restrict_xpaths='//span[@id="blog-pager-older-link"]'))
-    )
+     )
 
     def parse_patch(self, response):
-        date = response.xpath('//span[@class="post-timestamp"]/text()').extract()
-        change_type = response.xpath('//div[@class="post-body entry-content"]//b//text()').extract()
+
+        patch = response.xpath('//h1[@class="post-title entry-title"]/text()').extract()
+        patch = re.findall(r"[-+]?\d*\.\d+|\d+", patch[0])
+
+        # Patch date
+        date = response.xpath(
+            '//span[@class="post-timestamp"]/text()').extract()
+        dt = datetime.strptime(
+            date[0].replace('\n', ' '), ' %b %d, %Y %I:%M %p ')
+        release_date = time.mktime(dt.timetuple())
+
+        # Champ list
         champs = response.xpath('//div[@class="post-body entry-content"]//b//following-sibling::text()').extract()
+        while '\n' in champs: champs.remove('\n')
+        champs = [x.replace(' ', '').replace("'", '').replace("'", '').lower() for x in champs]
 
-        
-        
-        # # popularity
-        # d_pop = response.xpath(
-        #     './/*[@id="mainContent"]/div[2]/div[1]/div[1]/script').extract()
 
-        # # win rate
-        # d_wr = response.xpath(
-        #     './/*[@id="mainContent"]/div[2]/div[1]/div[2]/script').extract()
+        filename = 'patchnotes.txt'
 
-        # # ban rate
-        # d_br = response.xpath(
-        #     './/*[@id="mainContent"]/div[2]/div[1]/div[3]/script').extract()
+        # Save changes
+        with open(filename, 'a') as f:
+            s = str(patch[0]) + '\n'
+            f.write(s)
 
-        # # date released
-        # d_release = response.xpath(
-        #     './/*[@id="mainContent"]/div[1]/div[4]/div/div[1]/text()'
-        # ).extract()
+        with open(filename, 'a') as f:
+            s = str(release_date) + '\n'
+            f.write(s)
 
-        # filename = 'diamond_raw.txt'
+        changes = response.xpath(
+            '//div[@class="post-body entry-content"]//b//text()').extract()
+        change_track = 0
 
-        # with open(filename, 'a') as f:
-        #     s = champ_name + '\n^' + d_pop[0] + '\n^' + d_wr[0] + '\n^' + \
-        #         d_br[0] + '\n^' + d_release[0] + \
-        #         '\n|'
-        #     f.write(s)
-        # self.log('Saved file %s' % filename)
+        if any("buffs" in s.lower() for s in changes):
+            # Buffs
+            with open(filename, 'a') as f:
+                s = 'buffs:' + champs[0] + '\n'
+                f.write(s)
+                change_track += 1
+        if any("nerfs" in s.lower() for s in changes):
+            # Nerfs
+            with open(filename, 'a') as f:
+                s = 'nerfs:' + champs[change_track] + '\n'
+                f.write(s)
+
+        with open(filename, 'a') as f:
+            s = '|\n'
+            f.write(s)
+        self.log('Saved file %s' % filename)
+        print(str(patch[0]))
